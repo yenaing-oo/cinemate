8b6d31409a5f5e9b5acde61d62dc87c044d56745
Kelvin Oo <kelvinoo123@gmail.com>
2026-02-04 11:46:10 -0600

    Set up linting and formatting workflow with Prettier and ESLint configurations

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,10 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
-export default {
-  plugins: ["prettier-plugin-tailwindcss"],
+const config = {
+    plugins: ["prettier-plugin-tailwindcss"],
+    semi: true,
+    singleQuote: false,
+    tabWidth: 4,
+    trailingComma: "es5",
 };
+
+export default config;
