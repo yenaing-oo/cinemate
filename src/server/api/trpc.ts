88e6d584ca4f097643d020db84f26eea758feeda
Kelvin Oo <kelvinoo123@gmail.com>
2026-02-18 12:03:30 -0600

    Refactor protectedProcedure to use user authentication directly from context

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -128,20 +128,19 @@ export const publicProcedure = t.procedure.use(timingMiddleware);
  * Protected (authenticated) procedure
  *
  * If you want a query or mutation to ONLY be accessible to logged in users, use this. It verifies
- * the session is valid and guarantees `ctx.session.user` is not null.
+ * that the user is authenticated and throws an error if not.
  *
  * @see https://trpc.io/docs/procedures
  */
 export const protectedProcedure = t.procedure
     .use(timingMiddleware)
     .use(({ ctx, next }) => {
-        if (!ctx.session?.user) {
+        if (!ctx.user) {
             throw new TRPCError({ code: "UNAUTHORIZED" });
         }
         return next({
             ctx: {
-                // infers the `session` as non-nullable
-                session: { ...ctx.session, user: ctx.session.user },
+                user: ctx.user,
             },
         });
     });
