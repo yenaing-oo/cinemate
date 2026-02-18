6fbabbd72cf0b5db769781aaf3337ef9354fed59
Kelvin Oo <kelvinoo123@gmail.com>
2026-02-18 17:48:06 -0600

    Update logout button text from "Logout" to "Sign out"

--- src/components/logout-button.tsx
+++ src/components/logout-button.tsx
@@ -14,5 +14,5 @@ export function LogoutButton() {
         router.push("/auth/login");
     };
 
-    return <Button onClick={logout}>Logout</Button>;
+    return <Button onClick={logout}>Sign out</Button>;
 }
