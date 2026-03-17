e44b3fb766eb827170ad56f926edbae7e62be2f7
HrutilPatel <hrutilpatel7@gmail.com>
2026-03-17 11:57:14 -0500

    Update integration test

--- src/server/api/routers/movies.test.ts
+++ src/server/api/routers/movies.test.ts
@@ -52,6 +52,8 @@ describe("moviesRouter", () => {
                 genres: true,
                 runtime: true,
                 posterUrl: true,
+                backdropUrl: true,
+                description: true,
             },
             orderBy: { releaseDate: "desc" },
             // IMPORTANT: no "take" when limit is undefined
@@ -74,6 +76,8 @@ describe("moviesRouter", () => {
                 genres: true,
                 runtime: true,
                 posterUrl: true,
+                backdropUrl: true,
+                description: true,
             },
             orderBy: { releaseDate: "desc" },
             take: 5,
