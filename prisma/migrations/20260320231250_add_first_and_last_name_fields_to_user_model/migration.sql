82aa54a38fa3f7f204e6d2208ea7bca3d3e935d1
Kelvin Oo <kelvinoo123@gmail.com>
2026-03-20 18:30:40 -0500

    Add first and last name fields to User model and update sign-up form

---
+++ prisma/migrations/20260320231250_add_first_and_last_name_fields_to_user_model/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `name` on the `User` table. All the data in the column will be lost.
+  - Added the required column `firstName` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `lastName` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Made the column `supabaseId` on table `User` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "name",
+ADD COLUMN     "firstName" TEXT NOT NULL,
+ADD COLUMN     "lastName" TEXT NOT NULL,
+ALTER COLUMN "supabaseId" SET NOT NULL;
