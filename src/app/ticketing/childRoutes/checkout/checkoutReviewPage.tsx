40e17ad7e0c76546f8e78e85b81724be417bdd61
Hrutil <patelh23@myumanitoba.ca>
2026-04-07 01:35:04 -0500

    Remove the back button from seat selection and checkout page

--- src/app/ticketing/childRoutes/checkout/checkoutReviewPage.tsx
+++ src/app/ticketing/childRoutes/checkout/checkoutReviewPage.tsx
@@ -4,7 +4,6 @@ import { useRouter } from "next/navigation";
 import { $Enums } from "@prisma/client";
 import type { Decimal } from "@prisma/client/runtime/library";
 import { toast } from "sonner";
-import { BackButton } from "~/components/ui/back-button";
 import {
     formatCad,
     formatSeatFromCode,
@@ -110,27 +109,6 @@ export default function CheckoutReviewPage({
 
     return (
         <BookingReviewPanel
-            backButton={
-                <BackButton
-                    disabled={isSubmitting}
-                    onClick={() => {
-                        void handleUpdateSession(
-                            bookingSession.id,
-                            $Enums.BookingStep.SEAT_SELECTION
-                        ).catch((error) => {
-                            const message =
-                                error instanceof Error
-                                    ? error.message
-                                    : "Unable to go back to seat selection.";
-                            toast.error(message);
-                            router.replace("/ticketing");
-                            router.refresh();
-                        });
-                    }}
-                >
-                    Back to seats
-                </BackButton>
-            }
             movieTitle={movieTitle}
             showtimeLabel={showtimeLabel}
             posterUrl={moviePosterUrl}
